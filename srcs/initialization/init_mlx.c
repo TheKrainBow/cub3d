714edc27867d6fd13b292b25e62a011c3282cf01
TheKrainBow <thekrainbow@gmail.com>
2020-11-27 20:12:25 +0100

    norme

--- srcs/initialization/init_mlx.c
+++ srcs/initialization/init_mlx.c
@@ -6,7 +6,7 @@
 /*   By: magostin <magostin@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/10/09 05:02:08 by magostin          #+#    #+#             */
-/*   Updated: 2020/11/27 20:11:45 by magostin         ###   ########.fr       */
+/*   Updated: 2020/11/27 20:12:23 by magostin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,19 +15,19 @@
 void	init_gun_ptr(t_data *data)
 {
 	data->gun[0].ptr = mlx_xpm_file_to_image(data->mlx,
-	"./texture/Magic/0.xpm",&data->gun[0].wth, &data->gun[0].lth);
+	"./texture/Magic/0.xpm", &data->gun[0].wth, &data->gun[0].lth);
 	data->gun[1].ptr = mlx_xpm_file_to_image(data->mlx,
-	"./texture/Magic/1.xpm",&data->gun[1].wth, &data->gun[1].lth);
+	"./texture/Magic/1.xpm", &data->gun[1].wth, &data->gun[1].lth);
 	data->gun[2].ptr = mlx_xpm_file_to_image(data->mlx,
-	"./texture/Magic/2.xpm",&data->gun[2].wth, &data->gun[2].lth);
+	"./texture/Magic/2.xpm", &data->gun[2].wth, &data->gun[2].lth);
 	data->gun[3].ptr = mlx_xpm_file_to_image(data->mlx,
-	"./texture/Magic/3.xpm",&data->gun[3].wth, &data->gun[3].lth);
+	"./texture/Magic/3.xpm", &data->gun[3].wth, &data->gun[3].lth);
 	data->gun[4].ptr = mlx_xpm_file_to_image(data->mlx,
-	"./texture/Magic/4.xpm",&data->gun[4].wth, &data->gun[4].lth);
+	"./texture/Magic/4.xpm", &data->gun[4].wth, &data->gun[4].lth);
 	data->gun[5].ptr = mlx_xpm_file_to_image(data->mlx,
-	"./texture/Magic/5.xpm",&data->gun[5].wth, &data->gun[5].lth);
+	"./texture/Magic/5.xpm", &data->gun[5].wth, &data->gun[5].lth);
 	data->gun[6].ptr = mlx_xpm_file_to_image(data->mlx,
-	"./texture/Magic/6.xpm",&data->gun[6].wth, &data->gun[6].lth);
+	"./texture/Magic/6.xpm", &data->gun[6].wth, &data->gun[6].lth);
 	data->health.ptr = mlx_xpm_file_to_image(data->mlx,
 	"./texture/health_bar.xpm", &data->health.wth, &data->health.lth);
 }
