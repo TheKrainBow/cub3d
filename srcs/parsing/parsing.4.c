c0034de9c810b2a90aba043663cd7ee89a202842
TheKrainBow <thekrainbow@gmail.com>
2020-11-30 01:37:39 +0100

    norme

--- srcs/parsing/parsing.4.c
+++ srcs/parsing/parsing.4.c
@@ -6,9 +6,42 @@
 /*   By: magostin <magostin@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/30 01:33:06 by magostin          #+#    #+#             */
-/*   Updated: 2020/11/30 01:34:42 by magostin         ###   ########.fr       */
+/*   Updated: 2020/11/30 01:37:39 by magostin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cub3D.h"
 
+void	ft_create_player(t_data *data, int x, int y, char c)
+{
+	if (data->pars.player != 0)
+		aff_err("Multiple player definition.", data);
+	data->player.pos.x = x + 0.5;
+	data->player.pos.y = y + 0.5;
+	if (c == 'N')
+		data->player.angle = 270;
+	if (c == 'S')
+		data->player.angle = 90;
+	if (c == 'E')
+		data->player.angle = 0;
+	if (c == 'W')
+		data->player.angle = 180;
+	data->pars.player = 1;
+}
+
+void	ft_init_player(t_data *data)
+{
+	int		i;
+	int		j;
+
+	i = -1;
+	while (++i < data->game_size.x)
+	{
+		j = -1;
+		while (++j < data->game_size.y)
+		{
+			if (ft_strchr("NSWE", data->game[i][j]))
+				ft_create_player(data, j, i, data->game[i][j]);
+		}
+	}
+}
--- srcs/parsing/parsing.c
+++ srcs/parsing/parsing.c
@@ -6,7 +6,7 @@
 /*   By: magostin <magostin@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/07/06 12:03:50 by magostin          #+#    #+#             */
-/*   Updated: 2020/11/30 01:36:56 by magostin         ###   ########.fr       */
+/*   Updated: 2020/11/30 01:37:22 by magostin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -56,40 +56,6 @@ int		redirect_function(char *line, t_data *data)
 		return (0);
 	redirect[param](line, data);
 	return (0);
-}
-
-void	ft_create_player(t_data *data, int x, int y, char c)
-{
-	if (data->pars.player != 0)
-		aff_err("Multiple player definition.", data);
-	data->player.pos.x = x + 0.5;
-	data->player.pos.y = y + 0.5;
-	if (c == 'N')
-		data->player.angle = 270;
-	if (c == 'S')
-		data->player.angle = 90;
-	if (c == 'E')
-		data->player.angle = 0;
-	if (c == 'W')
-		data->player.angle = 180;
-	data->pars.player = 1;
-}
-
-void	ft_init_player(t_data *data)
-{
-	int		i;
-	int		j;
-
-	i = -1;
-	while (++i < data->game_size.x)
-	{
-		j = -1;
-		while (++j < data->game_size.y)
-		{
-			if (ft_strchr("NSWE", data->game[i][j]))
-				ft_create_player(data, j, i, data->game[i][j]);
-		}
-	}
 }
 
 void	check_parsing(t_data *data)
